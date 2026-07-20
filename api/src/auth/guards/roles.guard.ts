559b883cf1f342514383de7b252a8ba028c3a0f9
Yuriy <urabilik90@gmail.com>
2026-07-20 17:26:36 +0300

    fix role guard

--- api/src/auth/guards/roles.guard.ts
+++ api/src/auth/guards/roles.guard.ts
@@ -8,7 +8,10 @@ export class RolesGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles =
-      this.reflector.get<UserRole[]>('roles', context.getHandler()) ?? [];
+      this.reflector.getAllAndOverride<UserRole[]>('roles', [
+        context.getHandler(),
+        context.getClass(),
+      ]) ?? [];
     if (requiredRoles.length === 0) return true;
     const { user } = context.switchToHttp().getRequest();
     return user && requiredRoles.includes(user.role);
